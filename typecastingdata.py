ea229fc86d02c079db0eef21c978770a2e617ab2
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-05 11:00:36 +0000

    Add files via upload

---
+++ typecastingdata.py
@@ -0,0 +1,27 @@
+##convert float to int,str,bool
+a = 96.89
+print(int(a))
+print(str(a))
+print(bool(a))
+
+
+##convert int to float,str,bool
+a = 96
+print(float(a))
+print(str(a))
+print(bool(a))
+
+
+##convert bool to float,int,str
+a = True
+print(int(a))
+print(float(a))
+print(str(a))
+
+
+##convert str to int,float,bool
+a = "484"
+print(int(a))
+print(float(a))
+print(bool(a))
+
