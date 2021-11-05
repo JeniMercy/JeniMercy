088f1b31bc3c86c8ecf663f8a5eeeca9b8b74099
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-05 09:13:21 +0000

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,38 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+##convert float to int,str,bool
+a = 96.89
+print(int(a))
+96
+print(str(a))
+96.89
+print(bool(a))
+True
+
+##convert int to float,str,bool
+a = 96
+print(float(a))
+96.0
+print(str(a))
+96
+print(bool(a))
+True
+
+##convert bool to float,int,str
+a = True
+print(int(a))
+1
+print(float(a))
+1.0
+print(str(a))
+True
+
+##convert str to int,float,bool
+a = "484"
+print(int(a))
+484
+print(float(a))
+484.0
+print(bool(a))
+True
+
