223824b5d127996de3decbbfba3d79f4a9322e2e
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-07 12:00:23 +0000

    Add files via upload

---
+++ Nov6task.py
@@ -0,0 +1,27 @@
+a = "welcome*to#python&world!keep_learning^everyday"
+
+print(a[20])
+print(a[-16])
+print(a[21])
+print(a[-15])
+print(a[23])
+print(a[-13])
+print(a[31])
+print(a[-5])
+print(a[22])
+print(a[-14])
+print(a[30])
+print(a[-6])
+print(a[11])
+print(a[-25])
+
+"""
+calculate neg and pos index for below values
+*-> (a[20]) and (a[-39])
+p-> (a[11]) and (a[-35])
+p-> (a[27]) and (a[-9])
+&-> (a[17]) and (a[-29])
+^-> (a[37]) and (a[-9])
+%-> string index out of range
+"""
+
