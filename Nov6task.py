601b5885f2be1ff799bd88216c87c5eff1cd29be
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-07 13:45:31 +0000

    Add files via upload

--- Nov6task.py
+++ Nov6task.py
@@ -1,27 +1,27 @@
 a = "welcome*to#python&world!keep_learning^everyday"
 
-print(a[20])
-print(a[-16])
-print(a[21])
-print(a[-15])
-print(a[23])
-print(a[-13])
-print(a[31])
-print(a[-5])
-print(a[22])
-print(a[-14])
-print(a[30])
-print(a[-6])
-print(a[11])
-print(a[-25])
+print(a[20])#r
+print(a[-16])#e
+print(a[21])#l
+print(a[-15])#a
+print(a[23])#!
+print(a[-13])#n
+print(a[31])#a
+print(a[-5])#r
+print(a[22])#d
+print(a[-14])#r
+print(a[30])#e
+print(a[-6])#e
+print(a[11])#p
+print(a[-25])#l
 
-"""
+'''
 calculate neg and pos index for below values
-*-> (a[20]) and (a[-39])
-p-> (a[11]) and (a[-35])
-p-> (a[27]) and (a[-9])
-&-> (a[17]) and (a[-29])
-^-> (a[37]) and (a[-9])
-%-> string index out of range
-"""
+* #-> (a[20]) and (a[-39])
+p #-> (a[11]) and (a[-35])
+p #-> (a[27]) and (a[-9])
+& #-> (a[17]) and (a[-29])
+^ #-> (a[37]) and (a[-9])
+% #-> string index out of range
+'''
 
