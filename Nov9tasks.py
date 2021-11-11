1191f309453f10839de61b38339484bb15fdd390
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-11 01:53:47 -0800

    Update Nov9tasks.py

--- Nov9tasks.py
+++ Nov9tasks.py
@@ -3,57 +3,57 @@
 a = "Information_Technology_world"
 
 
-print(a[0:8])
-print(a[0:-8])
-print(a[-16:8])
-print(a[-16:-8])
-print(a[:8])
-print(a[:-8])
-print(a[9:16])
-print(a[9:100])
-print(a[9:])
-print(a[-7:])
-print(a[-7:16])
-print(a[-7:100])
-print(a[:])
-print(a[2:5])
-print(a[2:10])
-print(a[2:5:1])
-print(a[2:10:1])
-print(a[-15:-8:1])
-print(a[2:5:2])
-print(a[2:10:2])
-print(a[-15:-8:2])
-print(a[2:5:3])
-print(a[2:10:3])
-print(a[-15:-8:3])
-print(a[2:5])
-print(a[2:10])
-print(a[2:5:1])
-print(a[2:10:1])
-print(a[-15:-8:1])
-print(a[2:5:2])
-print(a[2:10:2])
-print(a[-15:-8:2])
-print(a[2:5:3])
-print(a[2:10:3])
-print(a[-15:-8:3])
-print(a[-14:15:2])
-print(a[12:15:1])
-print(a[-16:10:5])
-print(a[:10:2])
-print(a[:-8:3])
-print(a[:5:1])
-print(a[2::2])
-print(a[-10::2])
-print(a[12::3])
-print(a[13::6])
-print(a[10::100])
-print(a[2:10:1])
-print(a[2:10:-1])
-print(a[10:2:1])
-print(a[10:2:-1])
-print(a[-3:3:3])
-print(a[3:-3:3])
-print(a[-3:3:-3])
-print(a[3:-3:-3])
+print(a[0:8])       #Informat 
+print(a[0:-8])      #Information_Technolo
+print(a[-16:8])     # Empty
+print(a[-16:-8])    # Technolo
+print(a[:8])        # Informat
+print(a[:-8])       # Information_Technolo
+print(a[9:16])      # on_Tech
+print(a[9:100])     # on_Technology_world
+print(a[9:])        # on_Technology_world
+print(a[-7:])       # y_world
+print(a[-7:16])     # Empty
+print(a[-7:100])    # y_world
+print(a[:])         # Information_Technology_world
+print(a[2:5])       # for
+print(a[2:10])      # formatio
+print(a[2:5:1])     # for
+print(a[2:10:1])    # formatio
+print(a[-15:-8:1])  # echnolo
+print(a[2:5:2])     # fr
+print(a[2:10:2])    # frai
+print(a[-15:-8:2])  # ehoo
+print(a[2:5:3])     # f
+print(a[2:10:3])    # fmi
+print(a[-15:-8:3])  # eno
+print(a[2:5])       # for
+print(a[2:10])      # formatio
+print(a[2:5:1])     # for
+print(a[2:10:1])    # formatio
+print(a[-15:-8:1])  # echnolo
+print(a[2:5:2])     # fr
+print(a[2:10:2])    # frai
+print(a[-15:-8:2])  # ehoo
+print(a[2:5:3])     # f
+print(a[2:10:3])    # fmi
+print(a[-15:-8:3])  # eno
+print(a[-14:15:2])  # c
+print(a[12:15:1])   # Tec
+print(a[-16:10:5])  # Empty
+print(a[:10:2])     # Ifrai
+print(a[:-8:3])     # IoaoThl
+print(a[:5:1])      # Infor
+print(a[2::2])      # frainTcnlg_ol
+print(a[-10::2])    # lg_ol
+print(a[12::3])     # Thlyod
+print(a[13::6])     # eor
+print(a[10::100])   # n
+print(a[2:10:1])    # formatio
+print(a[2:10:-1])   # Empty
+print(a[10:2:1])    # noitamro
+print(a[10:2:-1])   # Empty
+print(a[-3:3:3])    # oaoThlyo
+print(a[3:-3:3])    # r_onentr
+print(a[-3:3:-3])   #Empty
+print(a[3:-3:-3])   #Empty
