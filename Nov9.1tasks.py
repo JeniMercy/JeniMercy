8b9ed4584320afa040e1cd420db5ed7a6df74d25
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-11 10:02:35 +0000

    Add files via upload

---
+++ Nov9.1tasks.py
@@ -0,0 +1,12 @@
+a = "hello_world_python"
+
+print(a[::3]) #hlwlph
+print(a[::-3]) #nt_r_l
+print(a[2::3]) #l_r_tn
+print(a[3::-3])#lh
+print(a[:5:-2])#nhy_lo
+print(a[:5:5]) #h
+print(a[::-1]) #nohtyp_dlrow_olleh 
+print(a[::1])  #hello_world_python
+print(a[::5])  #h_dh
+print(a[::-5]) #npol
