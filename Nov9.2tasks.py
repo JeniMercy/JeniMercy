08c6bbbe1d760362e3f9f7ad630dd09bdf6c0642
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-11 10:18:27 +0000

    Add files via upload

---
+++ Nov9.2tasks.py
@@ -0,0 +1,21 @@
+a = "Innovative_minds_python_group"
+
+print(a[-2:0:-1]) #uorg_nohtyp_sdnim_evitavonn
+print(a[5:-5:5])  #a_sh
+print(a[-5:5:-5]) #gtde
+print(a[5:-5:-5]) #empty
+print(a[3:9:-1])  #empty
+print(a[9:-10:-2])#empty
+print(a[2:-4:-4]) #empty
+print(a[-4:2:-2]) #r_otpsnmeiao
+print(a[0:-1:-3]) #empty
+print(a[-3:3:6])  #empty
+print(a[::-3])    #prnt_n_ivn
+print(a[::-2])    #pognhy_di_vtvnI
+print(a[2:-2:-2]) #empty
+print(a[2:-2:2])  #nvtv_id_yhngo
+
+
+
+
+
