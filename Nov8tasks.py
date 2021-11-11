b6b9ef62dc4640e7b383f1b3b4bfaafab7a56e4f
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-11 10:45:53 +0000

    Add files via upload

---
+++ Nov8tasks.py
@@ -0,0 +1,23 @@
+a = "Information_technology"
+
+print(a[10:15])#n_tec
+print(a[5:15])#mation_tec
+print(a[10:])#n_technology
+print(a[-15:-20])#empty
+print(a[-20:-15])#forma
+print(a[-3:-5])#Empty
+print(a[::2])#Ifraintcnlg
+print(a[10:17:2])#ntcn
+print(a[10:100:2])#ntcnlg
+print(a[-20:-15:3])#fm
+print(a[10:20:10])#n
+print(a[-20:-2:5])#ftto
+
+
+
+print(a[0:11])#Information
+print(a[12::])#technology
+print(a[11::2])#_ehooy
+print(a[0:5:2])#Ifr
+print(a[0:10:3])#Ioao
+
