a5e7a086f6020821eeb201397fab547ab442f2df
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-12 14:39:22 +0000

    Add files via upload

---
+++ Nov11Task1.py
@@ -0,0 +1,13 @@
+"""November 11:
+Strings:
+
+Task1:
+    
+#collect one string from user (input)
+#identify middle letter of the string
+"""
+
+a = input("enter the string : ")
+length = len(a)
+middle = length // 2
+print("The middle character is : " , a[middle])
