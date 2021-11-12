276a4f73e2af4767f8d8f3d720941d409a7cce28
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-12 16:51:36 +0000

    Add files via upload

---
+++ Nov11Task2.py
@@ -0,0 +1,29 @@
+"""Nov-11
+Task2:
+    
+Get two strings from user  example: 
+    "java" "groovy" ==> java6groovy4
+   
+   "pi" "mathematics"  ===> pi15mathematics2"""
+    
+
+a = input("enter the first string : ")
+b = input("enter the second string : ")
+n1 = int(input("enter the number1 : "))
+n2 = int(input("enter the number2 : "))
+print("{}{}{}{}".format(a,n1,b,n2))
+
+
+
+"""output:
+    enter the first string : jeni
+enter the second string : mercy
+enter the number1 : 3
+enter the number2 : 5
+jeni3mercy5
+"""
+
+
+
+
+
