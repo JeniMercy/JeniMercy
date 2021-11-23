7b3ee76b48d8b0081f1595016c1885b3afff4569
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-23 16:25:27 +0000

    Add files via upload

---
+++ Nov21task12.py
@@ -0,0 +1,20 @@
+#Nov21
+#Task12:
+
+#List #[123, 124, 125,130,131]
+#length of list odd or even
+
+
+List = [123, 124, 125, 130, 131]
+print(len(List))
+if len(List) % 2 == 0:
+    print("The list is even")
+else:
+    print("The list is odd")
+
+
+OUTPUT:
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21task12.py
+5
+The list is odd
+    
