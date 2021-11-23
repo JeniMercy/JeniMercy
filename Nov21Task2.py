60643534b8b88417cdfba87e47102e333453b0b2
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-23 16:15:08 +0000

    Add files via upload

---
+++ Nov21Task2.py
@@ -0,0 +1,29 @@
+#Nov-21
+#Task-2
+#collect one number from user, check whether the number is even or odd
+
+Number = int(input("Please enter the number : "))
+if Number % 2 == 0:
+    print("{} is Even Number " . format(Number))
+else:
+    print("{} is Odd Number " . format(Number))
+    
+
+
+
+"""OUTPUT:
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task2.py
+Please enter the number : 7650
+7650 is Even Number 
+
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task2.py
+Please enter the number : 0
+0 is Even Number 
+
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task2.py
+Please enter the number : -4
+-4 is Even Number 
+
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task2.py
+Please enter the number : 3567
+3567 is Odd Number """
