d3a0c93c1025b1908119d00d5557e0a3b4b7f3a1
Jeni J Mercy <83036171+JeniMercy@users.noreply.github.com>
2021-11-23 16:24:56 +0000

    Add files via upload

---
+++ Nov21Task6.py
@@ -0,0 +1,28 @@
+#Nov21
+#Task6:
+#get one string from user
+#check whether length of the string is odd or even
+
+
+a = input("Please enter your string : ")
+length = len(a)
+print("The length of string is",length)
+if length % 2 == 0:
+    print("The length of string is even")
+else:
+    print("The length of string is odd")
+    
+
+
+
+"""OUTPUT:
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task6.py
+Please enter your string : niralya
+The length of string is 7
+The length of string is odd
+
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python310/Nov21Task6.py
+Please enter your string : helllllloommnnbhgg
+The length of string is 18
+The length of string is even"""
+
